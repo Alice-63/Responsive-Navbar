1363eef79b4b296ca46e822a1f800b470a35f588
Alice-63 <alicetindag84@gmail.com>
2021-06-30 10:42:31 +0200

    created responsive navbar

---
+++ script.js
@@ -0,0 +1,48 @@
+var allA=document.getElementsByTagName("a");
+var nav=document.getElementById("nav");
+var a=false;
+
+function show()
+{
+    
+    if(a)
+    {
+        nav.style.height="54px"
+
+    }
+    else
+    {
+        nav.style.height="340px"
+    }
+   
+    
+    a=!a;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // if(a==2)
+    // {
+    //     for(i=0 ;i < allA.length ; i++)
+    // {
+    //     allA[i].style.display="none";
+    //     a=0;
+    // }
+
+    // }
